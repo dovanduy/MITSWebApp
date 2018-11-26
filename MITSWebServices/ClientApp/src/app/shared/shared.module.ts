1755f2bbf807b3c051db9e9a75a64116a57142d3
Jason <enderjs@gmail.com>
2018-11-26 17:47:32 -0600

    #17 - Add Angular Material and FlexLayout

---
+++ MITSWebServices/ClientApp/src/app/shared/shared.module.ts
@@ -0,0 +1,75 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import {
+  MatButtonModule, MatListModule, MatIconModule, MatCardModule, MatMenuModule, MatInputModule, MatButtonToggleModule,
+  MatProgressSpinnerModule, MatSelectModule, MatSlideToggleModule, MatDialogModule, MatSnackBarModule, MatToolbarModule,
+  MatTabsModule, MatSidenavModule, MatTooltipModule, MatCheckboxModule, MatRadioModule, MatGridListModule,
+  MatProgressBarModule, MatSliderModule, MatChipsModule, MatRippleModule, MatExpansionModule
+} from '@angular/material';
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatMenuModule,
+    MatInputModule,
+    MatButtonToggleModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatProgressBarModule,
+    MatSliderModule,
+    MatChipsModule,
+    MatGridListModule,
+    MatRippleModule,
+    MatExpansionModule,
+  ],
+  exports: [
+    CommonModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatCardModule,
+    MatMenuModule,
+    MatInputModule,
+    MatButtonToggleModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatProgressBarModule,
+    MatSliderModule,
+    MatChipsModule,
+    MatGridListModule,
+    MatRippleModule,
+    MatExpansionModule,
+  ]
+})
+export class SharedModule { }
